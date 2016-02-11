cb89ec7e71ec85f65ceab36234f374c87dffc9ea
benjelloun23 <anabenje@LFR008518.corp.capgemini.com>
2016-02-11 15:12:24 +0100

    init projet covoiturage maroc

---
+++ src/main/java/com/maroc/CovoiturageMarocApplication.java
@@ -0,0 +1,12 @@
+package com.maroc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovoiturageMarocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovoiturageMarocApplication.class, args);
+	}
+}
---
+++ src/test/java/com/maroc/CovoiturageMarocApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maroc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CovoiturageMarocApplication.class)
+public class CovoiturageMarocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
